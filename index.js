1b46fb96aee72a1eb786c4917349f311c2ece6fc
Awaiskhan17 <awaiskhan.jsx@gmail.com>
2024-02-17 21:07:05 +0500

    second commit

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 4000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.get('/naruto' , (req , res)=>{
+    res.send("Naruto is the best anima!")
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
